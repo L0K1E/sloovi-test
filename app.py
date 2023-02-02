3d79f2b32a5b30aba7f176defefe3990f579b474
LOKI <87218951+L0K1E@users.noreply.github.com>
2023-02-02 12:09:36 +0530

    Altered JWT condition & added extra error handling

--- app.py
+++ app.py
@@ -23,9 +23,8 @@ def login_required(f):
         
         if bearer != None:
             token = bearer.split()[1]
-
         else:
-            return jsonify({'message': 'Token not found / missing!'})
+            return jsonify({'message': 'Token not found / missing!'}), 203
 
         try:
             data = jwt.decode(token, app.config['SECRET_KEY'], algorithms="HS256")
@@ -34,7 +33,7 @@ def login_required(f):
         except jwt.ExpiredSignatureError:
             return jsonify({'message': 'Token is expired !, Login again to get new token.'}), 401
         except jwt.InvalidTokenError:
-            return jsonify({'message': 'Token is invalid !'}), 401
+            return jsonify({'message': 'Token is invalid !', 'tip 1': 'check whether the token is starts and ends with double qoutes, if so then remove it!', 'tip 2': 'login again and grab a new token and pass only the token along with the request.'}), 401
 
         return f(user, *args, **kwargs)
     return decorated 
@@ -42,40 +41,41 @@ def login_required(f):
 # Register User
 @app.route('/register', methods=['POST'])
 def create_user():
-    first_name = request.json['first_name']
-    last_name = request.json['last_name']
-    email = request.json['email']
-    password = request.json['password']
+    try:
+        first_name = request.json['first_name']
+        last_name = request.json['last_name']
+        email = request.json['email']
+        password = request.json['password']
+    except:
+        return jsonify({"Bad Request": "Incorrect entries!, you have made a request with missing fields or not in JSON structure."}), 400
 
     if db.users.find_one({'email': email}):
-        return jsonify({'message': 'email address is already taken'}), 401
+        return jsonify({'message': 'email address is already taken'}), 400
     
     if first_name and last_name and email and password:
         hashed_pass = generate_password_hash(password)
         id = db.users.insert_one(
             {'first_name': first_name,'last_name': last_name,'email': email,'password': hashed_pass})  
         response = jsonify({
-            'id': str(id),
-            'first_name': first_name,
-            'last_name': last_name,
-            'email': email,
-            'pasword': hashed_pass
+            'message' : 'User created successfully.',
+            'user': {
+            'id': str(id),'first_name': first_name,'last_name': last_name,'email': email,'pasword': hashed_pass }
         })
-        return response
-    
-    else:
-        return not_found()
+        return response, 201
 
 #login User
 @app.route('/login', methods=['POST'])
 def login_user():
-    email = request.json['email']
-    password = request.json['password']
-    
+    try:
+        email = request.json['email']
+        password = request.json['password']
+    except:
+        return jsonify({"Bad Request": "Incorrect entries!, you have made a request with missing fields or not in JSON structure."}), 400
+
     user = db.users.find_one({"email": email})
     
     if user and check_password_hash(user['password'], password):
-        token = jwt.encode({'email': user['email'], 'exp': datetime.datetime.utcnow() + datetime.timedelta(minutes=60)}, app.config['SECRET_KEY'], algorithm="HS256")
+        token = jwt.encode({'email': user['email'], 'exp': datetime.datetime.utcnow() + datetime.timedelta(minutes=14400)}, app.config['SECRET_KEY'], algorithm="HS256")
         response = jsonify({
             "message": "Login successfull",
             "token": token
@@ -93,21 +93,21 @@ def login_user():
 @app.route('/template', methods=['POST'])
 @login_required
 def create_template(user):
-    template_name = request.json['template_name']
-    subject = request.json['subject']
-    body = request.json['body']
+    try:
+        template_name = request.json['template_name']
+        subject = request.json['subject']
+        body = request.json['body']
+    except:
+        return jsonify({"Bad Request": "Incorrect entries!, you have made a request with missing fields or not in JSON structure."}), 400
     
     if template_name and subject and body:
         id = db.template.insert_one(
             {'user': user['email'],'template_name': template_name, 'subject': subject, 'body': body})  
         response = jsonify({
-            'id': str(id),
-            'user': user['email'],
-            'template_name': template_name,
-            'subject': subject,
-            'body': body
+            'message' : 'Template created successfully.',
+            'template': {'id': str(id), 'user': user['email'],'template_name': template_name, 'subject': subject,'body': body}
         })
-        return response
+        return response, 201
     else:
         return jsonify({"message": "Incorrect entries !"})
     
@@ -125,41 +125,47 @@ def all_template(user):
 @app.route('/template/<id>', methods=['GET'])
 @login_required
 def get_template(user, id):
-    template = db.template.find_one({'_id': ObjectId(id)})
-    response = json_util.dumps(template)
-    return Response(response, mimetype="application/json")
+    try:
+        template = db.template.find_one({'_id': ObjectId(id)})
+        response = json_util.dumps(template)
+        return Response(response, mimetype="application/json")
+    except:
+        return jsonify({"Bad Request": "Incorrect template ID!, you have passed a incorrect template ID as a request."}), 400
 
 # Delete template
 @app.route('/template/<id>', methods=['DELETE'])
 @login_required
 def delete_template(user, id):
-    template = db.template.find_one({'_id': ObjectId(id)})
-    if user['email'] == template['user']:
+    try:
         db.template.delete_one({'_id': ObjectId(id)})
         response = jsonify({'message': 'Template '+ id + ' deleted successfully !'}), 200
         return response
-    else:
-        return jsonify({'message': 'Unauthorized Access !, Only user who created the template can delete it.'}), 401
+    except:
+        return jsonify({"Bad Request": "Incorrect template ID!, you have passed a incorrect template ID as a request."}), 400
+
 
 #update template
 @app.route('/template/<id>', methods=['PUT'])
 @login_required
 def update_template(user, id):
-    template_name = request.json['template_name']
-    subject = request.json['subject']
-    body = request.json['body']
+    try:
+        template_name = request.json['template_name']
+        subject = request.json['subject']
+        body = request.json['body']
+    except:
+        return jsonify({"Bad Request": "Incorrect entries!, you have made a request with missing fields or not in JSON structure."}), 400
 
-    template = db.template.find_one({'_id': ObjectId(id)})
-    if user['email'] == template['user']:
+    try:
         if template_name and subject and body:
             db.template.update_one({'_id': ObjectId(id)}, {'$set': {'template_name': template_name, 'subject': subject, 'body': body}})  
             response = jsonify({'message': 'Template '+ id + ' updated successfully !'}), 200
             return response
         else:
             return jsonify({"message": "Incorrect entries !"})
-    else:
-        return jsonify({'message': 'Unauthorized Access !, Only user who created the template can update it.'}), 401
+    except:
+        return jsonify({"Bad Request": "Incorrect template ID!, you have passed a incorrect template ID as a request."}), 400
 
+#404 Not Found
 @app.errorhandler(404)
 def not_found(error=None):
     response = jsonify({
@@ -179,4 +185,4 @@ def home():
     '''
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True,port=5000)
